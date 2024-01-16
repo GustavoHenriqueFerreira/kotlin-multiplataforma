bf7f6a28edf217889b7b631efd8b72a090316e65
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2024-01-16 20:40:48 -0300

    feat: Finalizando configuracao das telas

--- vsconnect/app/src/main/java/com/senai/vsconnect/MainActivity.kt
+++ vsconnect/app/src/main/java/com/senai/vsconnect/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.senai.vsconnect
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.senai.vsconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -37,17 +36,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_editar_imagem, R.id.nav_servicos, R.id.nav_sair
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
