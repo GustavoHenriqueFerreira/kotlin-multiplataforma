9d12276e108ee63a9f9563e8ea85f857e1580fde
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2024-01-26 21:50:51 -0300

    feat: Comeco do consumo da rota de Login

--- vsconnect/app/src/main/java/com/senai/vsconnect_kotlin/apis/EndpointInterface.kt
+++ vsconnect/app/src/main/java/com/senai/vsconnect_kotlin/apis/EndpointInterface.kt
@@ -1,10 +1,17 @@
 package com.senai.vsconnect_kotlin.apis
 
+import com.google.gson.JsonObject
+import com.senai.vsconnect_kotlin.models.Login
 import com.senai.vsconnect_kotlin.models.Servico
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface EndpointInterface {
     @GET("servicos")
     fun listaServicos() : Call<List<Servico>> //função para receber uma lista de serviços
+
+    @POST("login")
+    fun login(@Body usuario: Login) : Call<JsonObject>
 }
